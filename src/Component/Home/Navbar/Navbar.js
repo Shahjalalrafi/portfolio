03a5b1452f4fe746cbc92733510bb87012286b63
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-05-02 21:13:59 +0600

    upload

--- src/Component/Home/Navbar/Navbar.js
+++ src/Component/Home/Navbar/Navbar.js
@@ -6,15 +6,15 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container flex">
-        <h1 className="logo"><Link to='/'>SHAHJALAL RAFI</Link></h1>
+        <h1 className="logo"><Link style={{textDecoration: "none", color: "white"}} to='/'>SHAHJALAL RAFI</Link></h1>
         <nav>
           <ul>
-            <Link to='/home'><li>home</li></Link>
-            <Link to='/about'><li>About</li></Link>
-            <Link to='/work'><li>Work</li></Link>
-            <Link to='/blog'><li>Blog</li></Link>
-            <Link to='/contact'><li>Contact</li></Link>
-            <a href="https://docs.google.com/document/d/1_gPW7vIzUMGcGkLoTkRW_o534uWfcHkhEHjcfO3zpU8/edit?usp=sharing" rel="noreferrer" target="_blank"><li>Resume</li></a>
+            <Link style={{textDecoration: "none"}} to='/home'><li>home</li></Link>
+            <Link style={{textDecoration: "none"}} to='/about'><li>About</li></Link>
+            <Link style={{textDecoration: "none"}} to='/work'><li>Work</li></Link>
+            <Link style={{textDecoration: "none"}} to='/blog'><li>Blog</li></Link>
+            <Link style={{textDecoration: "none"}} to='/contact'><li>Contact</li></Link>
+            <a style={{textDecoration: "none"}} href="https://docs.google.com/document/d/1_gPW7vIzUMGcGkLoTkRW_o534uWfcHkhEHjcfO3zpU8/edit?usp=sharing" rel="noreferrer" target="_blank"><li>Resume</li></a>
           </ul>
         </nav>
       </div>
