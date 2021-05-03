d263dbeb491d093be29c740866614df31694e93e
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-05-03 21:25:03 +0600

    upload

--- src/Component/About/About/About.js
+++ src/Component/About/About/About.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import Footer from '../../Home/Footer/Footer';
 import Navbar from '../../Home/Navbar/Navbar';
-import Skills from '../../Home/Skills/Skills';
+// import Skills from '../../Home/Skills/Skills';
+import styles from './About.module.css'
+import img from '../../../Images/pic1.png'
 
 const About = () => {
+    const skills = ['javascript', 'React.js', 'Node.js', 'Express.js', 'MongoDB.js', 'firebase', 'HTML', 'css', 'Bootstrap', 'material ul', 'Redux.js', 'sass']
+    const comfortable = ['javascript', 'React.js', 'Node.js', 'Express.js', 'MongoDB.js', 'Firebase', 'HTML', 'CSS', 'Bootstrap']
+    const familiar = ['redux', 'react-native', 'next.js']
     return (
         <div>
             <Navbar />
-            <Skills />
+            <div className={`${styles.container} ${styles.skills}`}>
+                <div data-aos="fade-right" className={`${styles.details}`}>
+                    <img style={{width: "70%"}} src={img} alt=""/>
+                </div>
+                <div data-aos="fade-left" className={styles.content}>
+                    <h4>My Skill Set</h4>
+                    <div>
+                        {
+                            skills.map(skill => <span>{skill}</span>)
+                        }
+                    </div>
+                    <h4 className={styles.comfort}>Feel free work with</h4>
+                    {
+                        comfortable.map(comfort => <span>{comfort}</span>)
+                    }
+
+                    <h4 className={styles.comfort}>Familiar with</h4>
+                    {
+                        familiar.map(fami => <span>{fami}</span>)
+                    }
+                </div>
+            </div>
             <Footer />
         </div>
     );
--- src/Component/ContactInfo/ContactDetails/ContactDetails.js
+++ src/Component/ContactInfo/ContactDetails/ContactDetails.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from './ContactDetails.module.css'
+import emailjs from 'emailjs-com';
 
 const ContactDetails = () => {
+
+    const sendMail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('gmail', 'template_tki0b9e', e.target, 'user_Idf5a9KWXITrEOeqp0y6D')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+
+        e.target.reset()
+    }
     return (
         <div className={`${styles.container} ${styles.contact}`}>
-            <form className={styles.form}>
-                <input className={styles.name} type="text" placeholder="Your Name" />
-                <input className={styles.email} type="email" placeholder="Your Email" />
-                <input className={styles.textArea} type="textarea" placeholder="Your Message" />
+            <form onSubmit={sendMail} className={styles.form}>
+                <input className={styles.name} type="text" name="name" placeholder="Your Name" />
+                <input className={styles.email} type="email" name="email" placeholder="Your Email" />
+                <input className={styles.textArea} type="textarea" name="message" placeholder="Your Message" />
                 <input type="submit" className={styles.btn} value="submit" />
             </form>
         </div>
